fa47ed4e8b3dc5fa5377ddb0c8951f0227f0b361
abhayjajodia <abhayjajodia86@gmail.com>
2025-06-12 14:11:17 +0300

    fixed deployment

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/useState_useEffect_practice/'
+  base: './',
+  build: {
+    assetsDir: './' // Put assets in root
+  }
 })
